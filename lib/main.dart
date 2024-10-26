c51391feae02247bcb83cb37844222743b1df79a
luhekawidiasi <luhekawidiasih3@gmail.com>
2024-10-27 00:06:09 +0800

    projekWidi

---
+++ lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          // Sidebar
+          Container(
+            width: 200,
+            color: Colors.blueGrey,
+            child: Column(
+              children: [
+                DrawerHeader(
+                  child: Text(
+                    'MENU',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.blueGrey,
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(Icons.home, color: Colors.white),
+                  title: Text('Home', style: TextStyle(color: Colors.white)),
+                ),
+                ListTile(
+                  leading: Icon(Icons.settings, color: Colors.white),
+                  title:
+                      Text('Settings', style: TextStyle(color: Colors.white)),
+                ),
+                ListTile(
+                  leading: Icon(Icons.contact_page, color: Colors.white),
+                  title: Text('Contact', style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+          ),
+          // Main Content Area (Expanded)
+          Expanded(
+            child: Column(
+              children: [
+                // Header
+                Container(
+                  height: 50,
+                  color: Colors.blue,
+                  child: Center(
+                    child: Text(
+                      'Data Psikolog',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                ),
+                // Content
+                Expanded(
+                  child: Container(
+                    color: Colors.white,
+                    child: Center(
+                      child: Text(
+                        'DATA',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
